774dcf78232cc0d64f77acf42ab3bc3e03f9b57b
Isadora Queiroz <KondoIqcDev@gmail.com>
2021-03-23 13:49:28 -0300

    Execução do exercício

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace grausFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double f=0;
+            double c=0;
+            Console.WriteLine("Convertendo graus Fahrenheit para graus Celsius. ");
+            Console.ForegroundColor=ConsoleColor.Blue;
+            Console.Write("Digite uma temperatura: ");
+            f=Convert.ToDouble(Console.ReadLine());
+            Console.ResetColor();
+            c= (f-32)/1.8;
+            Console.WriteLine("Tecle ENTER para ver o resultado.");
+            Console.ReadKey();
+            Console.ForegroundColor=ConsoleColor.Blue;
+            Console.WriteLine("Temperatura convertida em Celcius é: "+c);
+            Console.ResetColor();
+            Console.Beep();
+    
+        }
+    }
+}
